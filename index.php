7ff2a7a28648e1d5c6651aa7e81cbdb4a74ceeb5
Lydia Messaoui <55630251+LydiaMessaoui@users.noreply.github.com>
2021-11-20 00:02:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff" http-equiv="Content-Type" />
     <title>Connexion</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
